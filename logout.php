4ec808b9ed25dd6469c781abdc26e58dff642a18
dev001 <dev001@judobnb>
2016-07-19 19:17:40 +0900

    additional updates 7/19 2

--- logout.php
+++ logout.php
@@ -1,13 +1,23 @@
 <?php 
+define( 'BLOCK_LOAD', true );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-config.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php' );
+$wpdb = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
 
-include("wp-blog-header.php");
-$uriParts = explode('/',$_COOKIE['langss']);
+$uid = get_current_user_id();
+   
+$lang = $wpdb->get_var("SELECT value FROM `jd_bp_xprofile_data` WHERE field_id='635' AND user_id = $uid");
+ 
+if($lang=="Japanese"){
+     
+      $parts = "/ja/";
+      
+ }else{
+     
+      $parts = "";
+      
+ }
 
-if( $uriParts[3]== 'ja' )
-    $parts = "/ja/";
-else
-    $parts = "";
-	
 $redirect_to = 'index.php';
 $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : '';
 $location = str_replace('&amp;', '&', wp_logout_url($redirect_to."".$parts));
